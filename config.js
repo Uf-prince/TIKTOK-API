473c80602eff711afa73eafb5edd5338a4d90ada
UMAR • FAROOQ <ufprince1@gmail.com>
2025-10-30 12:30:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
 module.exports = {
     PORT: process.env.PORT || 3000,
-    TIKTOK_API_KEY: process.env.TIKTOK_API_KEY || "BilalTech05"
+    TIKTOK_API_KEY: process.env.TIKTOK_API_KEY || 'BilalTech05'
 };
